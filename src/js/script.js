a4c58f54849bba056b05f44ac5a3a294106a507a
Elang Alamsyah Habibi <154592963+elanghabibi@users.noreply.github.com>
2025-12-09 20:31:31 +0700

    update new portfolio 2.0

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,92 @@
-function mainPage(pageId) {
-  let mainContent = document.querySelectorAll(".main-content");
-  mainContent.forEach((p) => {
-    p.classList.remove("active");
-  });
-
-  let mainNav = document.querySelectorAll(".main-nav")
-  mainNav.forEach((n) => {
-    n.classList.remove("active")
-  })
-
-  let targetNav = document.querySelector(`.main-nav-${pageId}`)
-  targetNav.classList.add("active")
-
-  let targetContent = document.getElementById(pageId);
-  targetContent.classList.add("active");
+function toggleMenu() {
+  const navMobile = document.querySelector("#navMobile");
+  navMobile.classList.toggle("active");
 }
 
-function aboutPage(pageId) {
-  let aboutContent = document.querySelectorAll(".about-content");
-  aboutContent.forEach((p) => {
-    p.classList.remove("active");
-  });
+function myAge() {
+  const myBornYear = 2009;
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
 
-  let aboutNav = document.querySelectorAll(".about-nav")
-  aboutNav.forEach((n) => {
-    n.classList.remove("active")
-  })
+  const myAgeText = document.getElementById("myAge");
 
-  let targetNav = document.querySelector(`.about-nav-${pageId}`)
-  targetNav.classList.add("active")
+  const myAge = currentYear - myBornYear;
 
-
-  let targetContent = document.getElementById(pageId);
-  targetContent.classList.add("active");
+  myAgeText.textContent = myAge;
 }
+
+function updateKetDate() {
+  const currentDate = new Date();
+  const hours = currentDate.getHours();
+  const year = currentDate.getFullYear();
+
+  const ketWaktu = document.getElementById("ketWaktu");
+  ketWaktu.textContent = "";
+
+  if (hours >= 5 && hours <= 11) {
+    ketWaktu.textContent = "Morning";
+  } else if (hours >= 12 && hours <= 16) {
+    ketWaktu.textContent = "Afternoon";
+  } else if (hours >= 17 && hours <= 23) {
+    ketWaktu.textContent = "Evening";
+  } else if (hours >= 0 && hours <= 4) {
+    ketWaktu.textContent = "Evening";
+  }
+
+  const currentYearInf = document.getElementById("currentYearInf");
+  currentYearInf.textContent = year;
+}
+
+function goUp() {
+    const goUpBtn = document.getElementById('goUp');
+
+    window.addEventListener('scroll', function() {
+        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+            goUpBtn.classList.add('show')
+        } else {
+            goUpBtn.classList.remove('show')
+        }
+    })
+}
+
+goUp()
+
+myAge();
+updateKetDate();
+setInterval(updateKetDate, 1000);
+
+
+
+// Toggle Dark Mode
+const toggle = document.getElementById("toggleTheme");
+const toggleCircle = document.getElementById("toggleCircle");
+const html = document.documentElement;
+const toggleIcon = document.createElement("i");
+toggleIcon.classList.add("bx");
+
+// Cek apakah dark mode tersimpan
+if (localStorage.getItem("theme") === "dark") {
+  html.classList.add("dark");
+  toggleIcon.classList.remove("bx-sun");
+  toggleIcon.classList.add("bx-moon");
+} else {
+  html.classList.remove("dark");
+  toggleIcon.classList.remove("bx-moon");
+  toggleIcon.classList.add("bx-sun");
+}
+
+toggle.addEventListener("click", function () {
+  html.classList.toggle("dark");
+
+  if (html.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    toggleIcon.classList.remove("bx-sun");
+    toggleIcon.classList.add("bx-moon");
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleIcon.classList.remove("bx-moon");
+    toggleIcon.classList.add("bx-sun");
+  }
+});
+
+toggleCircle.appendChild(toggleIcon);
